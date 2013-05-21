2b184785b91511aa60f0265b8442c71c9ba6ef86
MoyTW <moytravis@gmail.com>
2013-05-21 15:14:00 -0700

    Okay now that it's sorted (though I'm still short on crucial understanding - yet I also want to progress! A quandry!) back to working on the Keybind object.

--- Python_Zappy/zappyui/Keybinds.py
+++ Python_Zappy/zappyui/Keybinds.py
@@ -1,49 +1,40 @@
 __author__ = 'Travis Moy'
 
-#from pyglet.window import key
-#from zappyui.Orders import ORDERS
-
-# OKAY. SO. Here's what I've got so far.
-# You can uncomment EITHER the __init__(self, binds="Blue!") def, OR the DEFAULT_KEYBINDS def. If you uncomment both,
-# it explodes, even if the init def doesn't reference the DEFAULT_KEYBINDS def.
-# Curiously, an __init__(self): taking no default parameters doesn't bug out. It can take a parameter, but if you make
-# a default value...
-
-# You can ALSO stop it from bugging out if you put a 'pass' (and nothing else) in DEFAULT_KEYBINDS, but...hmm.
-
-# So, ANY function in Keybinds which takes a default parameter, combined with the definition of DEFAULT_KEYBINDS which
-# does something, is causing the error.
-
-# I guess the question is "Why does what I do in Keybinds.py influence my testCameraSetLevel"?
-
-# Let's see if I can't narrow down what's going on in scratch_paper.
+from zappyui.Orders import ORDERS
+from pyglet.window import key
 
 
 def DEFAULT_KEYBINDS():
-    return dict()
+    binds = dict()
+    binds[key.UP] = ORDERS.UP
+    binds[key.RIGHT] = ORDERS.RIGHT
+    binds[key.LEFT] = ORDERS.LEFT
+    binds[key.DOWN] = ORDERS.DOWN
+    binds[key.ENTER] = ORDERS.CONFIRM
+    binds[key.BACKSPACE] = ORDERS.CANCEL
+    return binds
 
 
 # Transforms keypresses into orders
-class ZAPPY_Keybinds(object):
-    pass
+# Inputs map to orders. Inputs cannot be multiply bound.
+class Keybinds(object):
 
-    def function_with_default_param(self, fancifully_named_input="ZOO"):
-        self._thing = fancifully_named_input
+    def __init__(self, binds="DEFAULT_KEYBINDS"):
+        # Necessary to prevent default Keybind objects from interfering with each other.
+        # Not that I'm planning to have more than one, but never hurts to be flexible.
+        if binds == 'DEFAULT_KEYBINDS':
+            self._binds = DEFAULT_KEYBINDS()
+        else:
+            self._binds = binds
 
-    #def __init__(self, blue):
-    #    self.what = "WHAT"
-    #    self.blue = blue
+    def key_is_bound(self, key):
+        pass
 
-    #def __init__(self, blue=5):
-    #    self.what = "WHAT"
-    #    self.blue = blue
+    def bind_key(self, key, order):
+        pass
 
-    #def __init__(self, binds="Blue!"):
-    #    self._binds = binds
+    def unbind_key(self, key):
+        pass
 
-    #def __init__(self, binds=DEFAULT_KEYBINDS()):
-    #    self._binds = binds
-
-########################################################################################################################
-    #def handle_keys(self, symbol, modifiers):
-    #    pass
+    def handle_keys(self, symbol, modifiers):
+        pass
--- Python_Zappy/zappyui/testKeybinds.py
+++ Python_Zappy/zappyui/testKeybinds.py
@@ -1,27 +1,28 @@
 __author__ = 'Travis Moy'
 
 import unittest
-# IF YOU IMPORT zappyui.Keybinds, the test messes up.
-# ...okay. I'm confused, and how the heck do I diagnose what's going wrong?
 import zappyui.Keybinds
-#from pyglet.window import key
+from pyglet.window import key
+from zappyui.Orders import ORDERS
 
 
 class TestKeybinds(unittest.TestCase):
-
     def setUp(self):
-        pass
-        #self.default_keybinds = zappyui.Keybinds.Keybinds()
+        self.default_keybinds = zappyui.Keybinds.Keybinds()
 
     def tearDown(self):
-        pass
-        #self.default_keybinds = None
+        self.default_keybinds = None
 
-    def test_does_this_mess_up_tests(self):
-        pass
+    def test_default_keybinds(self):
+        binds = self.default_keybinds._binds
+        self.assertEqual(binds[key.LEFT], ORDERS.LEFT)
+        self.assertTrue(False)
 
-    def test_default_orders(self):
-        pass
+    def test_bind_key(self):
+        self.assertTrue(False)
+
+    def test_unbind_key(self):
+        self.assertTrue(False)
 
 suite = unittest.TestLoader().loadTestsFromTestCase(TestKeybinds)
 unittest.TextTestRunner(verbosity=2).run(suite)

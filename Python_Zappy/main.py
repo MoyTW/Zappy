9666f3472db64dccabd20db9c65358f9cd23272b
MoyTW <moytravis@gmail.com>
2013-05-23 16:36:35 -0700

    Done for now, trying to hack something out that I can actually interact with.

--- Python_Zappy/main.py
+++ Python_Zappy/main.py
@@ -1,6 +1,7 @@
 __author__ = 'Travis Moy'
 
 import pyglet
+from zappyui.UIScreens.UIScreenMenuBase import UIScreenMenuBase
 
 
 #width = 1920
@@ -9,5 +10,17 @@ width = 640 * 2
 height = 480 * 2
 window = pyglet.window.Window(width=width, height=height)
 print width, height
+
+class Viewport(object):
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+menu = UIScreenMenuBase(Viewport(width, height))
+
+@window.event
+def on_draw():
+    window.clear()
+    menu.draw()
 
 pyglet.app.run()
--- Python_Zappy/zappyui/UIScreens/UIScreenMenuBase.py
+++ Python_Zappy/zappyui/UIScreens/UIScreenMenuBase.py
@@ -6,8 +6,29 @@ import pyglet
 
 # This is the Base screen, which is at the bottom of this history.
 class UIScreenMenuBase(UIScreen.UIScreen):
-    
+    IMAGE_PATH = 'images/base_menu/'
+    loader = pyglet.resource.Loader('[@assets]')
+    background_image = loader.image(IMAGE_PATH + 'background.png')
+    exit_image = loader.image('exit.png')
+    levels_image = loader.image('levels.png')
+    options_image = loader.image('options.png')
+    player_image = loader.image('player.png')
+    select_frame_image = loader.image('select_frame.png')
+
+    def __init__(self, window_viewport, selection=1):
+        self._viewport = window_viewport
+        self._selection = selection
+
+        self._center_pixel = (self._viewport.width / 2, self._viewport.height / 2)
+
+    def _init_sprites(self):
+        self._background_sprite = pyglet.sprite.Sprite(self.background_image, x=self._center_pixel[0] -
+                                                       self.background_image.width / 2)
+
     def handle_order(self, order):
+        pass
+
+    def _change_selection(self, order):
         pass
 
     def draw(self):

d58baa840db8d49d75249b9cf0a59ad8b157e102
MoyTW <moytravis@gmail.com>
2013-06-25 16:00:11 -0700

    Okay. Why in the world does the new LoaderLevel class squash the preview?

--- Python_Zappy/main.py
+++ Python_Zappy/main.py
@@ -4,6 +4,7 @@ import pyglet
 from zappyui.UIController import UIController
 from zappyui.FactoryScreens import FactoryScreens
 from loader.oldLoaderLevel import oldLoaderLevel
+from loader.LoaderLevelV1 import LoaderLevelV1
 
 #width = 1920
 #height = 640
@@ -18,7 +19,8 @@ height = 480
 
 window = pyglet.window.Window(width=width, height=height)
 
-loader_level = oldLoaderLevel()
+#loader_level = oldLoaderLevel()
+loader_level = LoaderLevelV1()
 factory = FactoryScreens(window, loader_level)
 
 uicontrol = UIController(window, factory.create_ScreenMenuBase())

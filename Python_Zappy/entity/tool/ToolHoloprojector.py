8fd30891d6d2dfa4ba8324c06de11dc8db31b3de
MoyTW <moytravis@gmail.com>
2013-07-01 13:51:01 -0700

    Fixed bug with improper naming of tool.
    The parameter for determining the name of the hologram created by the tool
    was overwriting the tool's _entity_name, causing it to not be initialized
    properly. Renamed parameter, data member to prevent this.

--- Python_Zappy/entity/tool/ToolHoloprojector.py
+++ Python_Zappy/entity/tool/ToolHoloprojector.py
@@ -16,8 +16,8 @@ import entity.actor.effects.EffectDeath as EffectDeath
 # distracting the enemies.
 class ToolHoloprojector(Tool.Tool):
 
-    def __init__(self, _level, _entity_name='Hologram', _hp=999, _threat=9, **kwargs):
-        self._entity_name = _entity_name
+    def __init__(self, _level, _holo_name='Hologram', _hp=999, _threat=9, **kwargs):
+        self._holo_name = _holo_name
         self._hp = _hp
         self._threat = _threat
         kwargs['_list_target_types'] = [self.TYPE_LOCATION]
@@ -26,7 +26,7 @@ class ToolHoloprojector(Tool.Tool):
 
     def _effects_of_use_on_location(self, _x, _y):
         user_faction = self._user.get_faction()
-        holo = Actor.Actor(self._level, _entity_name=self._entity_name, _max_hp=self._hp, _faction=user_faction,
+        holo = Actor.Actor(self._level, _entity_name=self._holo_name, _max_hp=self._hp, _faction=user_faction,
                            _base_threat=self._threat)
         holo.apply_status_effect(EffectDeath.EffectDeath(5, holo))
         self._level.place_entity_at(holo, _x, _y)
